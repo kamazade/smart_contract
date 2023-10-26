b1869fbee41a6dfae001f830d00fc7de40fc5902
Alper KAMA <92380837+kamazade@users.noreply.github.com>
2023-10-26 13:26:43 +0300

    Update NFTMinter.sol

--- smart-contract/NFTMinter.sol
+++ smart-contract/NFTMinter.sol
@@ -6,14 +6,42 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTMinter is ERC721URIStorage, Ownable {
     uint256 private tokenIdCounter;
+    uint256 public nextTokenId;
+    uint256 public royaltyPercentage;
+    address public treasuryWallet;
+    address public liquidityWallet;
 
-    constructor(address initialOwner) ERC721("MyNFT", "NFT") Ownable(initialOwner) {}
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint256 _royaltyPercentage , 
+        address _liquidityWallet, 
+        address _treasuryWallet
+    ) ERC721(_name, _symbol) Ownable(msg.sender) { 
+        nextTokenId = 1; 
+        royaltyPercentage = _royaltyPercentage; 
+        liquidityWallet = _liquidityWallet; 
+        treasuryWallet = _treasuryWallet; 
+    }
 
-    function mintNFT(address recipient, string memory tokenURI) external onlyOwner {
-        uint256 tokenId = tokenIdCounter;
-        tokenIdCounter += 1;
-        _mint(recipient, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+    function mintNFT(string memory _tokenURI, uint256 _price) external payable {
+        require(_price > 0, "Price Must Be Greater Than 0 ");
+        require(msg.value >= _price, "Insufficient Funds");
+
+        uint256 tokenId = nextTokenId;
+        nextTokenId++;
+
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
+        uint256 royaltyAmount = (_price * royaltyPercentage) / 100;
+        uint256 liquidityAmount = (_price * (10 - royaltyPercentage)) / 100;
+
+        (bool success, ) = treasuryWallet.call{value: royaltyAmount}("");
+        require(success, "Treasury Wallet Transfer Failed");
+
+        (success, ) = liquidityWallet.call{value: liquidityAmount}("");
+        require(success, " Liquidity Wallet Transfer Failed");
     }
 }
 
